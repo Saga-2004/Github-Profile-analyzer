f3d82b0e5448a70a20f84ab4413828ebe573eee8
Sagar <msagars2008@gmail.com>
2026-06-15 15:35:18 +0530

    Fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,8 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: [
-    process.env.FRONTEND_URL,
-    'http://localhost:5173'
+    process.env.FRONTEND_URL || 'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'DELETE'],
   allowedHeaders: ['Content-Type']
